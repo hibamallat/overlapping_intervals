96b0b7cc198c4e628bda912b6ca020c3ff623a41
hibamallat <hibamallat040@gmail.com>
2024-11-09 16:03:54 +0200

    Add file

---
+++ index.py
@@ -0,0 +1,6 @@
+"""
+Write a function merge_intervals(intervals), where intervals is an array of arrays
+(for example: intervals = [ [1,3], [2,4], [6,8]). The functions merges overlapping 
+intervals and returns a list of the merged intervals. In the case of the intervals 
+array passed earlier, the result should be [ [1, 4], [6,8] ]
+"""
