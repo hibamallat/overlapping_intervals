c77c76f9c99b097c5172868f461446c7878e6ce9
hibamallat <hibamallat040@gmail.com>
2024-11-09 16:09:18 +0200

    Implement first code version

--- index.py
+++ index.py
@@ -4,3 +4,27 @@ Write a function merge_intervals(intervals), where intervals is an array of arra
 intervals and returns a list of the merged intervals. In the case of the intervals 
 array passed earlier, the result should be [ [1, 4], [6,8] ]
 """
+array = [[1,3] , [2,4] , [6,8]]
+
+def merge_intervals(intervals):
+
+    intervals = sorted(intervals)
+
+    merged_list = [] 
+
+    for i in range (len(intervals) - 1):
+
+        start_index = intervals[i + 1][0]
+
+        end_index = intervals[i][1]
+
+
+        if  start_index <= end_index  :
+            merged_list.append([intervals[i][0] , intervals[i + 1][1]])
+        else:
+            merged_list.append(intervals[i])
+
+                
+    return merged_list
+print(array)
+print(merge_intervals(array))
