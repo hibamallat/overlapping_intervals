1d413fa4d530760d8bd03eded377f41c2aa2549a
hibamallat <hibamallat040@gmail.com>
2024-11-12 03:07:58 +0200

    Test code: 1 case passes, 1 case fails

--- index.py
+++ index.py
@@ -5,24 +5,28 @@ intervals and returns a list of the merged intervals. In the case of the interva
 array passed earlier, the result should be [ [1, 4], [6,8] ]
 """
 array = [[1,3] , [2,4] , [6,8]]
+#array = [[1,5] , [2,6] , [8,10] , [9,12]]
 
 def merge_intervals(intervals):
 
-    intervals = sorted(intervals)
+    #intervals = sorted(intervals)
 
     merged_list = [] 
 
     for i in range (len(intervals) - 1):
 
-        start_index = intervals[i + 1][0]
+        start_index = intervals[i+1][0]
 
         end_index = intervals[i][1]
 
-
-        if  start_index <= end_index  :
-            merged_list.append([intervals[i][0] , intervals[i + 1][1]])
+        if  start_index <= end_index :
+            merged_list.append([intervals[i][0] , intervals[i+1][1]])
         else:
-            merged_list.append(intervals[i])
+            merged_list.append(intervals[i+1])
+        
+        
+        
+            
 
                 
     return merged_list
